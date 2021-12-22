6e52c151c47d7622e51ffa3b534ac52c8d7f9518
Irene <78106075+DonaIrene@users.noreply.github.com>
2021-12-22 14:27:24 +0000

    move up

--- RPG/player.py
+++ RPG/player.py
@@ -2,7 +2,8 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,pos):
         super().__init__()
-        self.image = 
-        self.rect =   
+        self.image = pygame.surface.Surface((1,2))
+        self.image.fill ('red')
+        self.rect =   self.image.get_rect(topleft = pos)
